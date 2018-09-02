de6231b2c5e6cb8901d8b36a52f1a749c9c67a6e
Ruben Hamelink <ruben.hamelink@hotmail.com>
2018-09-02 18:26:22 +0200

    Add project files.

---
+++ ModelToObj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelToObj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
